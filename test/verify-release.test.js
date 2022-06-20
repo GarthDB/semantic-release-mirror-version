c45a085ec197896671b2f99b109e12c2f03b0b8c
Garth Braithwaite <garthdb@gmail.com>
2022-06-20 17:21:16 -0600

    test: verify version number is added to the file

--- test/verify-release.test.js
+++ test/verify-release.test.js
@@ -1,23 +1,31 @@
 const test = require("ava");
 const { stub } = require("sinon");
 const verifyRelease = require("../index").verifyRelease;
-const fs = require("fs");
+const { existsSync } = require("fs");
+const {
+  readFile,
+  writeFile,
+  copyFile,
+  rm,
+  mkdir,
+  access,
+} = require("fs/promises");
 
 const expected = {};
 
-test.beforeEach((t) => {
+test.beforeEach(async (t) => {
   const log = stub();
   t.context.log = log;
   t.context.logger = { log };
-  if (!fs.existsSync("./test/tmp/")) fs.mkdirSync("./test/tmp/");
+  if (!existsSync("./test/tmp/")) await mkdir("./test/tmp/");
 });
 
-test.afterEach((t) => {
-  fs.rmSync("./test/tmp/", { force: true, recursive: true });
+test.afterEach(async (t) => {
+  await rm("./test/tmp/", { force: true, recursive: true });
 });
 
 test("Verify release with default RegExp", async (t) => {
-  fs.copyFileSync("./test/fixtures/basic.json", "./test/tmp/basic.json");
+  await copyFile("./test/fixtures/basic.json", "./test/tmp/basic.json");
   t.deepEqual(
     await verifyRelease(
       { fileGlob: "./test/tmp/basic.json" },
@@ -25,11 +33,13 @@ test("Verify release with default RegExp", async (t) => {
     ),
     []
   );
-  t.true(t.context.log.calledWith("Some message from plugin."));
+  const result = await readFile("./test/tmp/basic.json", { encoding: "utf8" });
+  t.deepEqual(JSON.parse(result), { version: "12.0.0-beta.34" });
+  // t.true(t.context.log.calledWith("Some message from plugin."));
 });
 
 test("Verify release with custom RegExp", async (t) => {
-  fs.copyFileSync("./test/fixtures/regex.json", "./test/tmp/regex.json");
+  await copyFile("./test/fixtures/regex.json", "./test/tmp/regex.json");
   t.deepEqual(
     await verifyRelease(
       { fileGlob: "./test/tmp/regex.json", placeholderRegExp: /0\.0\.0-dev/g },
@@ -37,5 +47,24 @@ test("Verify release with custom RegExp", async (t) => {
     ),
     []
   );
-  t.true(t.context.log.calledWith("Some message from plugin."));
+  const result = await readFile("./test/tmp/basic.json", { encoding: "utf8" });
+  t.deepEqual(JSON.parse(result), { version: "12.0.0-beta.34" });
+  // t.true(t.context.log.calledWith("Some message from plugin."));
+});
+
+test("Verify release with custom RegExp with matching group", async (t) => {
+  await copyFile("./test/fixtures/regex.json", "./test/tmp/regex.json");
+  t.deepEqual(
+    await verifyRelease(
+      {
+        fileGlob: "./test/tmp/regex.json",
+        placeholderRegExp: /"version": ?"(?<version>[^"]*)"/g,
+      },
+      { nextRelease: { version: "12.0.0-beta.34" }, logger: t.context.logger }
+    ),
+    []
+  );
+  const result = await readFile("./test/tmp/basic.json", { encoding: "utf8" });
+  t.deepEqual(JSON.parse(result), { version: "12.0.0-beta.34" });
+  // t.true(t.context.log.calledWith("Some message from plugin."));
 });
