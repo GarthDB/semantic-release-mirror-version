3dca02f84ada702a37088ce427faa26f81817373
Garth Braithwaite <garthdb@gmail.com>
2022-06-21 17:05:29 -0600

    test: add tests for logging

--- test/verify-release.test.js
+++ test/verify-release.test.js
@@ -1,70 +1,93 @@
 const test = require("ava");
 const { stub } = require("sinon");
 const verifyRelease = require("../index").verifyRelease;
-const { existsSync } = require("fs");
-const {
-  readFile,
-  writeFile,
-  copyFile,
-  rm,
-  mkdir,
-  access,
-} = require("fs/promises");
+const { existsSync, mkdirSync, rmSync } = require("fs");
+const { randomBytes } = require("crypto");
+const { readFile, writeFile, copyFile, access } = require("fs/promises");
 
 const expected = {};
+
+test.before(async (t) => {
+  if (existsSync("./test/tmp/")) {
+    await rmSync("./test/tmp/", { force: true, recursive: true });
+  }
+});
 
 test.beforeEach(async (t) => {
   const log = stub();
   t.context.log = log;
   t.context.logger = { log };
-  if (!existsSync("./test/tmp/")) await mkdir("./test/tmp/");
+  if (!existsSync("./test/tmp/")) {
+    await mkdirSync("./test/tmp/");
+  }
 });
 
 test.afterEach(async (t) => {
-  await rm("./test/tmp/", { force: true, recursive: true });
+  await rmSync("./test/tmp/", { force: true, recursive: true });
 });
 
 test("Verify release with default RegExp", async (t) => {
-  await copyFile("./test/fixtures/basic.json", "./test/tmp/basic.json");
+  const filename = "./test/tmp/basic.json";
+  await copyFile("./test/fixtures/basic.json", filename);
   t.deepEqual(
     await verifyRelease(
-      { fileGlob: "./test/tmp/basic.json" },
-      { nextRelease: { version: "12.0.0-beta.34" }, logger: t.context.logger }
+      { fileGlob: filename },
+      { nextRelease: { version: "12.1.2-beta.43" }, logger: t.context.logger }
     ),
     []
   );
-  const result = await readFile("./test/tmp/basic.json", { encoding: "utf8" });
-  t.deepEqual(JSON.parse(result), { version: "12.0.0-beta.34" });
-  // t.true(t.context.log.calledWith("Some message from plugin."));
+  const basic = JSON.parse(
+    await readFile(filename, {
+      encoding: "utf8",
+    })
+  );
+  t.snapshot(basic);
+  t.true(t.context.log.calledWith("Matched fileGlob files: 1"));
+  t.true(t.context.log.calledWith(`File ${filename} match index: 16`));
 });
 
 test("Verify release with custom RegExp", async (t) => {
-  await copyFile("./test/fixtures/regex.json", "./test/tmp/regex.json");
-  t.deepEqual(
-    await verifyRelease(
-      { fileGlob: "./test/tmp/regex.json", placeholderRegExp: /0\.0\.0-dev/g },
-      { nextRelease: { version: "12.0.0-beta.34" }, logger: t.context.logger }
-    ),
-    []
-  );
-  const result = await readFile("./test/tmp/basic.json", { encoding: "utf8" });
-  t.deepEqual(JSON.parse(result), { version: "12.0.0-beta.34" });
-  // t.true(t.context.log.calledWith("Some message from plugin."));
-});
-
-test("Verify release with custom RegExp with matching group", async (t) => {
-  await copyFile("./test/fixtures/regex.json", "./test/tmp/regex.json");
+  const filename = "./test/tmp/regex.json";
+  await copyFile("./test/fixtures/regex.json", filename);
   t.deepEqual(
     await verifyRelease(
       {
-        fileGlob: "./test/tmp/regex.json",
-        placeholderRegExp: /"version": ?"(?<version>[^"]*)"/g,
+        fileGlob: filename,
+        placeholderRegExp: /0\.0\.0-dev/g,
       },
       { nextRelease: { version: "12.0.0-beta.34" }, logger: t.context.logger }
     ),
     []
   );
-  const result = await readFile("./test/tmp/basic.json", { encoding: "utf8" });
-  t.deepEqual(JSON.parse(result), { version: "12.0.0-beta.34" });
-  // t.true(t.context.log.calledWith("Some message from plugin."));
+  const result = JSON.parse(
+    await readFile(filename, {
+      encoding: "utf8",
+    })
+  );
+  t.snapshot(result);
+  t.true(t.context.log.calledWith("Matched fileGlob files: 1"));
+  t.true(t.context.log.calledWith(`File ${filename} match index: 16`));
+});
+
+test("should log when no strings match", async (t) => {
+  const filename = "./test/tmp/regex-no-match.json";
+  await copyFile("./test/fixtures/regex.json", filename);
+  t.deepEqual(
+    await verifyRelease(
+      {
+        fileGlob: filename,
+        placeholderRegExp: /(?<="ver": *")[^"]*/g,
+      },
+      { nextRelease: { version: "12.0.0-beta.34" }, logger: t.context.logger }
+    ),
+    []
+  );
+  const result = JSON.parse(
+    await readFile(filename, {
+      encoding: "utf8",
+    })
+  );
+  t.snapshot(result);
+  t.true(t.context.log.calledWith("Matched fileGlob files: 1"));
+  t.true(t.context.log.calledWith(`File ${filename} match index: -1`));
 });
